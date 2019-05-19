e2553ef8fcfecb097d3c15b0e28df94a440092b1
arthurgeay <arthurgeay.contact@gmail.com>
2019-05-19 16:47:52 +0200

    Ajout des étapes pour planter une plante

--- includes/_plant-flower.php
+++ includes/_plant-flower.php
@@ -30,3 +30,36 @@ unset($_SESSION['error']);
 
     <input class="btn btn-success" type="submit" name="plant-flower" />
 </form>
+
+<hr>
+
+<h2 class="mt-4 text-center">Comment planter votre nouvelle plante ?</h2>
+
+<h3>Matériels nécessaire</h3>
+
+<ul>
+    <li>Un crayon de papier</li>
+    <li>Pot en terre cuite</li>
+    <li>Terreau ou de la terre du jardin</li>
+    <li>Gravier ou billes d'argiles</li>
+    <li>Graines pour votre plante</li>
+    <li>Arrosoir</li>
+</ul>
+
+<h3>Etapes pour planter votre plante</h3>
+
+<ol>
+    <li>Remplir votre pot de terreau</li>
+    <ul>
+        <li>Placez votre pot en terre cuite sous une soucoupe</li>
+        <li>Versez au fond du pot du gravier ou des billes d'argiles (environ 3cm)</li>
+        <li>Remplissez le pot de terreau ou de terre du jardin</li>
+    </ul>
+    <li>Plantez la graine</li>
+    <ul>
+        <li>Faites un trou de 3 à 4 cm avec le crayon de papier dans le terreau</li>
+        <li>Déposez 3 graines dans le trou, afin de s'assurer qu'une des graines germe au minimum</li>
+        <li>Rebouchez le trou à la main et tassez le terreau</li>
+        <li>Arrosez l'ensemble du pot</li>
+    </ul>
+</ol>
